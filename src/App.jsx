677b59a831d3da6e3033029f7364e09876cd1788
Achmad Furqon Rachmadie <uuqkun@gmail.com>
2023-11-27 19:13:42 +0700

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import { MovieList, TvShows, Navbar } from "./components";
+import { MovieList, TvShows, Navbar, MovieDetails } from "./components";
 
 function App() {
   return (
@@ -8,8 +8,11 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route index path="/" element={<MovieList />} />
-          <Route path="/tv-shows" element={<TvShows />} />
+          <Route path="/" element={<h1>Welcome</h1>} />
+          <Route index path="/movies" element={<MovieList />} />
+          <Route path="/movies/:id" element={<MovieDetails />} />
+          <Route path="/tv-shows" element={<TvShows />} />"
+          <Route path="*" element={<h1>Not Found</h1>} />
         </Routes>
       </Router>
     </main>
