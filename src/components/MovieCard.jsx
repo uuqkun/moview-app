b5a70b50707c7724d7f68921b2f781a894dd37d7
Achmad Furqon Rachmadie <uuqkun@gmail.com>
2023-11-27 19:14:49 +0700

    add custom route

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const MovieCard = ({ data }) => {
   return (
-    <article className="moviecard">
-      <img src={data.image} alt={data.title} className="moviecard__img" />
-      <div className="moviecard__texts">
-        <h5 className="gray_200">{data.title}</h5>
-      </div>
-      <span className="moviecard__rating">
-        <p>
-          {data.rating}
-        </p>
-      </span>
-    </article>
+    <Link to={`/movies/${data.imdbid}`}>
+      <article className="moviecard">
+        <img src={data.image} alt={data.title} className="moviecard__img" />
+        <div className="moviecard__texts">
+          <h5 className="gray_200">{data.title}</h5>
+        </div>
+        <span className="moviecard__rating">
+          <p>{data.rating}</p>
+        </span>
+      </article>
+    </Link>
   );
 };
 
