0606e425f8f2791aba3f09e1e73231d2b9266524
Achmad Furqon Rachmadie <uuqkun@gmail.com>
2023-11-28 09:16:46 +0700

    created card details page

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -5,10 +5,59 @@ const MovieDetails = () => {
   const { movies, imdbid } = useMovieManager();
 
   const selectedMovie = movies.filter((movie) => movie.imdbid === imdbid)[0];
+  const isMovieAvailable =
+    selectedMovie !== undefined || selectedMovie !== null;
+
   return (
-    <div>
-      <h1 className="gray_50">{selectedMovie.title}</h1>
-    </div>
+    <>
+      {isMovieAvailable ? (
+        <main className="details">
+          <div className="details__hero">
+            <img src={selectedMovie.big_image} alt={selectedMovie.title} />
+          </div>
+
+          <figure className="details__body">
+            <div className="details__top">
+              <div className="details__title">
+                <p>Moview / Movies</p>
+                <h3>{selectedMovie.title}</h3>
+              </div>
+            </div>
+
+            <div className="details__bottom">
+              <img src={selectedMovie.image} alt={selectedMovie.title} />
+
+              <figcaption>
+                <h5>Part of the journey is the end.</h5>
+                <p className="body-large gray_200">
+                  {selectedMovie.description}
+                </p>
+
+                <p className="details__rating body-large">
+                  <span>star</span>
+                  {selectedMovie.rating}
+                </p>
+
+                <div className="details__attribute">
+                  <h6>Release Date</h6>
+                  <p className="body-large">{selectedMovie.year}</p>
+                </div>
+                <div className="details__attribute">
+                  <h6>Run Time</h6>
+                  <p className="body-large">130 Min</p>
+                </div>
+                <div className="details__attribute">
+                  <h6>Genre(s)</h6>
+                  <p className="body-large">{selectedMovie.genre}</p>
+                </div>
+              </figcaption>
+            </div>
+          </figure>
+        </main>
+      ) : (
+        <p>Movie not found</p>
+      )}
+    </>
   );
 };
 
