c4df79c1b494517de35b3f748c6580d05ca5074d
Achmad Furqon Rachmadie <uuqkun@gmail.com>
2023-11-28 07:08:48 +0700

    add new routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { MovieList, TvShows, Navbar, MovieDetails } from "./components";
+import { StoreProvider } from "./services/store";
 
 function App() {
   return (
-    <main>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<h1>Welcome</h1>} />
-          <Route index path="/movies" element={<MovieList />} />
-          <Route path="/movies/:id" element={<MovieDetails />} />
-          <Route path="/tv-shows" element={<TvShows />} />"
-          <Route path="*" element={<h1>Not Found</h1>} />
-        </Routes>
-      </Router>
-    </main>
+    <StoreProvider>
+      <main>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<h1>Welcome</h1>} />
+            <Route index path="/movies" element={<MovieList />} />
+            <Route path="/movies/:id" element={<MovieDetails />} />
+            <Route path="/tv-shows" element={<TvShows />} />"
+            <Route path="*" element={<h1>Not Found</h1>} />
+          </Routes>
+        </Router>
+      </main>
+    </StoreProvider>
   );
 }
 
