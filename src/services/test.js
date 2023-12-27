a5a50fd0780ce56eec55a57f970207ee90ae4d75
Achmad Furqon Rachmadie <uuqkun@gmail.com>
2023-12-27 21:41:37 +0700

    modified

--- src/services/test.js
+++ src/services/test.js
@@ -1,1 +1,18 @@
 console.log(test)
+// Assuming you have an array of movies
+const movies = [
+    { name: "The Shawshank Redemption", genre: "Drama" },
+    { name: "The Godfather", genre: "Crime" },
+    { name: "Pulp Fiction", genre: "Crime" },
+    { name: "The Dark Knight", genre: "Action" },
+    { name: "Fight Club", genre: "Drama" },
+];
+
+// Function to filter movies by name
+function filterMoviesByName(name) {
+    return movies.filter(movie => movie.name.toLowerCase().includes(name.toLowerCase()));
+}
+
+// Example usage
+const filteredMovies = filterMoviesByName("dark");
+console.log(filteredMovies);
